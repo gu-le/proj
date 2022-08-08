5c7a4cf9eb8d804f3287443d22225ab629f8d606
gorsing <gorsing@localhost.localdomain>
2022-08-08 23:36:29 +0300

    fix

--- book/models.py
+++ book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
- 
+# 
 # Create your models here.
 class Book(models.Model):
     title=models.CharField(max_length=200,null=True)
@@ -10,6 +10,7 @@ class Book(models.Model):
     Publisher=models.CharField(max_length=30,null=True)
     SeriesBooks=models.CharField(max_length=30,null=True)
     Genre=models.CharField(max_length=20,null=True)
+    Image = models.ImageField(blank=True, upload_to='book_images')
     def __str__(self):
         return str(self.title)
  
