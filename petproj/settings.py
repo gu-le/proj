1ef226a17d4715e0205e894bd2c1a1f6d426c4a6
gu-le <gu-le@mail.ru>
2022-08-15 04:34:42 +0300

    add accaunts

--- petproj/settings.py
+++ petproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tkf6wfh_-gbdsqzung_=2(-ne#mu@1%bh&=*uw0y5mwuklyj8w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gule.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['gule.pythonanywhere.com','127.0.0.1','0.0.0.0']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book',
+    'books',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
